d23afe3de5112683990769b75997eaea23eb6a6e
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-04 03:58:31 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+// switch 
+switch(color){
+    case 'green':
+        console.log('You are a green friend');
+        break;
+    case 'blue':
+        console.log('You are a BLUE friend');
+        break;
+    case 'white':
+        console.log('You are a white friend');
+        break;
+    case 'red':
+        console.log('You are a red friend');
+        break;
+    case 'yellow':
+        console.log('You are a himu friend');
+        break;
+    default:
+        console.log('You are a kala kala friend');
+}
