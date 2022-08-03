0bfd4e9c780865a023ee198fdf68568c5c1c9982
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-04 03:23:14 +0600

    object

---
+++ object.js
@@ -0,0 +1,19 @@
+// object declaration
+var student = {
+    id: 477470,
+    name: 'sabbir',
+    class: '5th semester',
+    marks: 3.86
+}
+console.log(student.class);
+
+// 
+var mobile = {
+    brand: 'tacno',
+    peice: 1500,
+    storage: '128gb',
+    camera: '16/48 MP'
+}
+console.log(mobile);
+mobile.storage = '256gb';
+console.log(mobile);
