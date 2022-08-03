26b8ad0f55ff756d109ab1e6a445d7cdb90ce376
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-04 01:19:47 +0600

    pramiter

--- pramiter.js
+++ pramiter.js
@@ -1,5 +1,28 @@
+// simple pramiter declaration
 function bringSingra(money) {
     console.log('mama singra dan');
     console.log(money);
 }
 bringSingra(250);
+
+// more explore pramiter
+function bring(money) {
+    console.log('taka disen:', money);
+    console.log('mama singra dan');
+}
+var taka = 300;
+bring(taka);
+
+//more explore pramiter 2 added
+function add(num1, num2) {
+    console.log('going add:', num1, num2);
+}
+add(121, 212);
+
+// more explore pramiter kisu number sum
+function sum(a, b, c, d, e,) {
+    console.log(a, b, c, d, e,);
+    var sumAdd = a + b + c + d + e;
+    console.log(sumAdd);
+}
+sum(23, 54, 64, 87, 56, 32);
