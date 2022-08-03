fa70b1e4cd027ec5e438ede52a5930802d5dc1b9
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-04 00:35:16 +0600

    function explore

---
+++ funtion.js
@@ -0,0 +1,8 @@
+// function declaration
+function startFan() {
+    console.log('start up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+// call the function
+startFan();
