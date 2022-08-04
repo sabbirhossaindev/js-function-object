c79dc6267ae08facfe0c71c73aef45db3ec2559e
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-04 12:53:26 +0600

    reacp object property

--- set-propertes.js
+++ set-propertes.js
@@ -7,12 +7,26 @@ var soppingCard = {
 }
 console.log(soppingCard);
 
-// get the property value
+// 1.get the property value
 // var penCount = shoppingCart.pen;
-// alternative System get the property value
+
+// 2.alternative System get the property value
 // var penCount2 = shoppingCart['pen'];
 
 var properties = Object.keys(soppingCard); // just keys gula output dibaaa
 console.log(properties);
 var propertiesValue = Object.values(soppingCard); // just values gula output dibaaa
 console.log(propertiesValue);
+
+// 3.variable diya property & value get the property value
+var propertyName = 'mouse';
+var propertiesValue = soppingCard[propertyName];
+console.log(propertyName, propertiesValue);
+
+// set property values
+shoppingCart.mouse = 15; // step 1
+console.log(shoppingCart);
+shoppingCart['mouse'] = 29;// step 2
+console.log(shoppingCart);
+shoppingCart[propertyName] = 89;// step 3
+console.log(shoppingCart);
